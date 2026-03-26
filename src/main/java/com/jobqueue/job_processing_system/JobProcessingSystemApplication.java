b0f3b2356a495cd13df9019cfe64ab61f4081e11
tushardhawas <tushardhawasssd@gmail.com>
2026-03-26 21:21:43 +0530

    Initial commit - Spring Boot setup

---
+++ src/main/java/com/jobqueue/job_processing_system/JobProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jobqueue.job_processing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobProcessingSystemApplication.class, args);
+	}
+
+}
